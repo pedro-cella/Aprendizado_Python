73fbbdf630ed8b36d72b15420e4dce1468b26f8d
Pedro Cella <pedrovscella@gmail.com>
2021-11-01 20:11:46 -0300

    Finalizando modulo 2

---
+++ SQL/Alura/MySQL-Avancado/Aula9-Modulo2.sql
@@ -0,0 +1,6 @@
+SELECT * FROM tabela_de_produtos WHERE SABOR LIKE '%Maca%';
+
+SELECT * FROM tabela_de_produtos WHERE SABOR LIKE '%Maca%'
+AND EMBALAGEM = 'PET';
+
+SELECT * FROM tabela_de_clientes WHERE NOME LIKE 'E%';
