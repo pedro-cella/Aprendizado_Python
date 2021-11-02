c27d2403aef52d05ab69a4fbf68f6fba33e2259d
Pedro Cella <pedrovscella@gmail.com>
2021-11-01 21:25:19 -0300

    Continuando modulo 3

--- SQL/Alura/MySQL-Avancado/Aula3-Modulo3.sql
+++ SQL/Alura/MySQL-Avancado/Aula3-Modulo3.sql
@@ -9,4 +9,3 @@ SELECT * FROM tabela_de_produtos LIMIT 0, 3;
 SELECT * FROM notas_fiscais;
 
 SELECT * FROM notas_fiscais WHERE DATA_VENDA IN ('2017-01-01') LIMIT 10;
-
---
+++ SQL/Alura/MySQL-Avancado/Aula5-Modulo3.sql
@@ -0,0 +1,17 @@
+SELECT * FROM tabela_de_produtos;
+
+SELECT * FROM tabela_de_produtos ORDER BY PRECO_DE_LISTA;
+
+SELECT * FROM tabela_de_produtos ORDER BY PRECO_DE_LISTA DESC;
+
+SELECT * FROM tabela_de_produtos ORDER BY NOME_DO_PRODUTO;
+
+SELECT * FROM tabela_de_produtos ORDER BY NOME_DO_PRODUTO DESC;
+
+SELECT * FROM tabela_de_produtos ORDER BY EMBALAGEM, NOME_DO_PRODUTO;
+
+SELECT * FROM tabela_de_produtos ORDER BY EMBALAGEM DESC, NOME_DO_PRODUTO ASC;
+
+SELECT * FROM tabela_de_produtos WHERE NOME_DO_PRODUTO IN ('Linha Refrescante - 1 Litro - Morango/Limão') ORDER BY NOME_DO_PRODUTO;
+
+SELECT * FROM itens_notas_fiscais WHERE CODIGO_DO_PRODUTO = '1101035' ORDER BY QUANTIDADE DESC; 
