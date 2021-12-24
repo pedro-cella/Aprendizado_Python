0596a57cf1d0bb01efa95e2b6298aae2187a776c
Pedro Cella <pedrovscella@gmail.com>
2021-12-24 08:49:10 -0300

    Exercicio 1066 URI

---
+++ Python/URI/ex1066.py
@@ -0,0 +1,20 @@
+pares = 0
+impares = 0
+positivos = 0
+negativos = 0
+for i in range(0, 5):
+    numero = int(input())
+    if numero % 2 == 0:
+        pares += 1
+    else:
+        impares += 1
+
+    if numero > 0:
+        positivos += 1
+    elif numero < 0:
+        negativos += 1
+
+print(f"{pares} valor(es) par(es)")
+print(f"{impares} valor(es) impar(es)")
+print(f"{positivos} valor(es) positivo(s)")
+print(f"{negativos} valor(es) negativo(s)")
